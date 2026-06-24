de056c7f86511ac1e7d47bc0d92027bb3c934891
whyitsmara <maferperez20122@gmail.com>
2026-06-24 01:14:41 -0500

    vistas

--- src/main/java/com/cibertec/hospital/controller/RefundPolicyController.java
+++
@@ -1,8 +0,0 @@
-package com.cibertec.hospital.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@RequestMapping("/api/refund-policy")
-public class RefundPolicyController {
-
-}
